12c506c2efaf815d0ff9e726b87897d3d7af0ec4
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2024-01-06 16:54:49 +0600

    packages added

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+export const config = {
+    appwriteApiEndpoint: String(import.meta.env.VITE_APPWRITE_API_ENDPOINT),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
